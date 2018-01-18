b76ff84c4b0e71c5ff3e6258f14391422d974af3
ksz <kmlszopa@gmail.com>
2018-01-18 23:58:16 +0100

    first commit

---
+++ src/main/java/pl/kamilszopa/schoolregister/SchoolRegisterApplication.java
@@ -0,0 +1,12 @@
+package pl.kamilszopa.schoolregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolRegisterApplication.class, args);
+	}
+}
